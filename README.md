acbb1e6fe5f7ff1910cb62a0ce3ef3a3833b1187
Anthony Ou <ou.anthony@gmail.com>
2018-12-06 00:50:49 -0700

    add npm data

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple node compressor
+
+A very simple brotli and gzip compress that requires no build tools to use.
--- index.js
+++ index.js
@@ -1,27 +1,42 @@
+#!/usr/bin/env node
+
 const { compress } = require('wasm-brotli');
 const fs = require('fs-extra');
 const zlib = require('zlib');
 const glob = require('glob');
 const commander = require('commander');
+const mime = require('mime-types');
 
 commander.option('-f, --folder [folder]').parse(process.argv);
 
-glob(`./${commander.folder}/**/@(js|css|html)`, async (err, files) => {
-  const results = files.map(async file => {
-    console.log(file);
-    const content = await fs.readFile(file);
-    const [br, gz] = await Promise.all([
-      compress(content),
-      new Promise((res, reject) => {
-        zlib.gzip(content.toString(), (err, buf) => {
-          res(buf);
-        });
-      }),
-    ]);
-    return Promise.all([
-      fs.writeFile(file + '.br', br),
-      fs.writeFile(file + '.gz', gz),
-    ]);
-  });
+const acceptedTypes = [
+  'text/html',
+  'text/css',
+  'image/svg+xml',
+  'application/javascript',
+  'application/json',
+];
+glob(`${commander.folder}/**/*`, async (err, files) => {
+  const results = files
+    .filter(file => {
+      const a = acceptedTypes.includes(mime.lookup(file));
+      console.log(file, mime.lookup(file));
+      return a;
+    })
+    .map(async file => {
+      const content = await fs.readFile(file);
+      const [br, gz] = await Promise.all([
+        compress(content),
+        new Promise((res, reject) => {
+          zlib.gzip(content.toString(), (err, buf) => {
+            res(buf);
+          });
+        }),
+      ]);
+      return Promise.all([
+        fs.writeFile(file + '.br', br),
+        fs.writeFile(file + '.gz', gz),
+      ]);
+    });
   await Promise.all(results);
 });
